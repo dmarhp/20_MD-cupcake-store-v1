2b282b570699f756a9ee3498e7df14759e66508d
dmarhp <dmarhp@gmail.com>
2021-11-04 18:37:34 +0200

    alert

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,12 @@
 import "./Button.css"
 
 const Button = (props) => {
+    const clickHandler = () =>{
+        alert("You choose " + props.buttonName);
+    }
 
     return (
-        <button className="button">{props.buttonName}</button>
+        <button className="button" onClick={clickHandler}>{props.buttonName}</button>
     );
 }
 
